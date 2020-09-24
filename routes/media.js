041a7fd45b3750c6eb0a09460af94240bed70236
rartstudio <58383924+rartstudio@users.noreply.github.com>
2020-09-24 23:39:02 +0700

    integrasi api part 3 (delete image)

--- routes/media.js
+++ routes/media.js
@@ -9,5 +9,6 @@ const mediaHandler = require('./handler/media');
 
 router.post('/',mediaHandler.create);
 router.get('/',mediaHandler.getAll);
+router.delete('/:id',mediaHandler.destroy);
 
 module.exports = router;
